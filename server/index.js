d4f5017f3e50bbaedee43d851afff692b4585b88
JaylinJones0 <jonesjaylink@gmail.com>
2025-12-18 12:29:12 -0600

    (remove) helmet from dependency list in package.json & the explanation for installing in server/index.js

--- server/index.js
+++ server/index.js
@@ -1,24 +1,6 @@
 const express = require("express");
 const axios = require("axios");
-/* Since we're most likely unfamiliar since we haven't used
- helmet in class here's a brief reason for installing helmet:
 
- - It's stated to be best practice as is directly referenced in
- Express's 'Production Best Practices: Security' section which I will reference here: 
- https://expressjs.com/en/advanced/best-practice-security.html#production-best-practices-security
-
- - I (Jaylin) came across helmet when running into a Content Security Policy error in the 
- browser when attempting to click the rendered hyperlinks that link directly to the job posting.
- For this singular issue helmet is not required, *however* we may run into issues down the line
- that helmet can solve in terms of security for our application, and general errors that may show
- themselves in the browser's console if there's a HTTP header that we forgot to set/configure.
-
- ONLY CONCERN WITH HELMET:
- - MAY or may not need to disable header if it clashes with our needs. 
- For most cases helmet should be a set it and forget it solution. Luckily 
- disabiling or custom configuration of headers is possible with helmet.
-*/
-const helmet = require("helmet");
 
 const dotenv = require("dotenv/config");
 const { app_id, app_key } = process.env;
@@ -37,7 +19,6 @@ const app = express();
 const port = 3000;
 
 //MIDDLEWARE:
-app.use(helmet());
 app.use(express.static(path.resolve(__dirname, "../dist")));
 app.use(express.json());
 app.use(express.urlencoded());
