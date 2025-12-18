b2417f175aa56274b2d92a3857cd9cdc0f71a8db
JaylinJones0 <jonesjaylink@gmail.com>
2025-12-17 23:17:06 -0600

    (add) ability to render individual job listings from API

--- client/components/SuggestedJobList.jsx
+++ client/components/SuggestedJobList.jsx
@@ -1,10 +1,15 @@
-import React from 'react';
+import React from "react";
 
-import SuggestedListEntry from './SuggestedListEntry.jsx';
+import SuggestedListEntry from "./SuggestedListEntry.jsx";
 
 export default function SuggestedJobList({ jobs }) {
-
-    return (
-        <SuggestedListEntry jobs={jobs}/>
-    )
+  return (
+    <div>
+        {
+            jobs.map(job => {
+                return <SuggestedListEntry name={job.title} link={job.redirect_url} description={job.description} key={job.id} />
+            })
+        }
+    </div>
+  );
 }
--- client/components/SuggestedListEntry.jsx
+++ client/components/SuggestedListEntry.jsx
@@ -1,8 +1,11 @@
-import React from 'react';
+import React from "react";
 
-export default function SuggestedListEntry({ jobs }) {
-
-    return (
-        <div>placeholder</div>
-    );
+export default function SuggestedListEntry({ name, link, description }) {
+  return (
+    <div>
+        <p>Job Title: {name} </p>
+        <a href='url'>{link}</a>
+        <p>Description: {description}</p>
+    </div>
+  );
 }
