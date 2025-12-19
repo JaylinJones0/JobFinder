c3de27afa53f03d6f37a133077a241980aa999e6
JaylinJones0 <jonesjaylink@gmail.com>
2025-12-18 19:02:20 -0600

    (add) ability to filter SuggestedJobList with multiple interests, very minor syntax change to /findjobs/:category endpoint

--- client/components/FindJobs.jsx
+++ client/components/FindJobs.jsx
@@ -2,10 +2,10 @@ import React from 'react';
 
 import SuggestedJobList from './SuggestedJobList.jsx';
 
-export default function FindJobs({ jobs }) {
+export default function FindJobs({ jobs, getJobListings }) {
 
     return (
-        <SuggestedJobList jobs={jobs}/>
+        <SuggestedJobList jobs={jobs} getJobListings={getJobListings}/>
     );
 
 }
--- client/components/SuggestedJobList.jsx
+++ client/components/SuggestedJobList.jsx
@@ -1,15 +1,75 @@
 import React from "react";
-
+import { useState, useEffect, useEffectEvent } from "react";
 import SuggestedListEntry from "./SuggestedListEntry.jsx";
 
-export default function SuggestedJobList({ jobs }) {
+export default function SuggestedJobList({ jobs, getJobListings }) {
+  const [selectedPrefs, setSelectedPrefs] = useState(new Set());
+  
+  console.log(selectedPrefs)
+
+  const getDefaultJobListingsEvent = useEffectEvent(() => {
+    getJobListings();
+    return; // do nothing, end function (page would initally load very slow without return)
+  });
+
+  const getJobListingsEvent = useEffectEvent(() => {
+    // loop won't run until preferences are provided
+    for (const pref of selectedPrefs) {
+      let prefsArray = Array.from(selectedPrefs);
+      getJobListings(pref, prefsArray);
+    }
+    return; // do nothing, end function (page would initally load very slow without return)
+  });
+
+  // function that runs on initial render
+  // also runs every time selectedPrefs is updated
+  useEffect(() => {
+    if (selectedPrefs.size === 0) {
+      getDefaultJobListingsEvent();
+    }
+    // if selectedPrefs isn't empty
+    getJobListingsEvent();
+  }, [selectedPrefs]);
+
   return (
     <div>
-        {
-            jobs.map(job => {
-                return <SuggestedListEntry name={job.title} link={job.redirect_url} description={job.description} key={job.id} />
-            })
-        }
+      <h1>Choose your job preferences</h1>
+      <div className="job-category-buttons">
+        <div>
+          <button
+            name="it-jobs"
+            onClick={(e) => {
+              const category = e.target.name;
+              setSelectedPrefs(new Set([...selectedPrefs, category]));
+            }}
+          >
+            IT
+          </button>
+        </div>
+        <div>
+          <button
+            name="engineering-jobs"
+            onClick={(e) => {
+              const category = e.target.name;
+              setSelectedPrefs(new Set([...selectedPrefs, category]));
+            }}
+          >
+            Engineering
+          </button>
+        </div>
+      </div>
+      <div className="list-entry">
+        {jobs.map((job) => {
+          return (
+            <SuggestedListEntry
+              name={job.title}
+              link={job.redirect_url}
+              description={job.description}
+              key={job.id}
+            />
+          );
+        })}
+      </div>
     </div>
   );
 }
